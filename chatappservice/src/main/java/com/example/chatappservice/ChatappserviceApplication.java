cde41d729225179c4c7bc7bf5b56bfe5ec58f961
vibhore tiwari <vibhore.tiwari@coraltele.com>
2024-10-01 12:20:56 +0530

    initialise project

---
+++ chatappservice/src/main/java/com/example/chatappservice/ChatappserviceApplication.java
@@ -0,0 +1,12 @@
+package com.example.chatappservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatappserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatappserviceApplication.class, args);
+	}
+}
---
+++ chatappservice/src/test/java/com/example/chatappservice/ChatappserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatappservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatappserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
